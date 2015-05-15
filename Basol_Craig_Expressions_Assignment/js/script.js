44c638c80706daed074c295da50fe442ff5609ca
Craig Basol <cbasol@fullsail.edu>
2015-05-14 20:06:53 -0400

    Added Intro Code
    Prompt for user name and return description of program

--- Basol_Craig_Expressions_Assignment/js/script.js
+++ Basol_Craig_Expressions_Assignment/js/script.js
@@ -6,3 +6,13 @@ Section 01
 Expressions Assignment
 */
 
+// Calculator to determine cost for filling a tank of gas and whether to drive further for a cheaper price
+
+// Ask for the user's name
+var name = prompt("Before we begin, could you tell me your name?");
+// log to console
+console.log(name);
+
+// Introduce calculator to user
+alert("Hello "+name+", let's see how much it will be to fill up your gas tank and whether you should drive further for a better price.");
+
