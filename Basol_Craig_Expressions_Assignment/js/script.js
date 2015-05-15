9e9781a5a72966bd0a6b076c97725437ca466d87
Craig Basol <cbasol@fullsail.edu>
2015-05-14 21:35:08 -0400

    Clean Up and Test Values
    Cleaned up some code; forced rounding on additional results for easier
    reading; included test values

--- Basol_Craig_Expressions_Assignment/js/script.js
+++ Basol_Craig_Expressions_Assignment/js/script.js
@@ -22,7 +22,7 @@ var tankSize = prompt('How many gallons can your gas tank hold?');
 console.log(tankSize);
 
 // Prompt for amount of gas left
-var tankAmnt = prompt('As a decimal, how much gas do you have left? [e.g. About half a tank = 0.50');
+var tankAmnt = prompt('As a decimal, how much gas do you have left? [e.g. About half a tank = 0.50]');
 // log to console
 console.log(tankAmnt);
 
@@ -59,8 +59,8 @@ var gasBurn = distance / mileage;
 // log to console
 console.log(gasBurn);
 
-// Calculate new fill amount needed and store difference
-var gasDiff = gasBurn;
+// Log additional gas used and calculate new amount needed to fill
+var gasDiff = gasBurn.toFixed(2);
 gasBurn += gasFill;
 // log to console
 console.log(gasBurn);
@@ -82,10 +82,24 @@ costTwo = costTwo.toFixed(2);
 console.log(costOne);
 console.log(costTwo);
 
-// Calculate difference in price
+// Calculate and round difference in price
 var difference = Math.abs(costOne - costTwo);
+difference = difference.toFixed(2);
 // log to console
 console.log(difference);
 
 // Deliver final results to user
 alert('Thanks '+name+'! Let\'s take a look at what we\'ve come up with. Filling up at the first station will cost you $'+costOne+'. If you drive to the second gas station, you will burn an additional '+gasDiff+' gallon(s) of fuel. The cost to fill up at the second station will be $'+costTwo+'. That\'s a difference of $'+difference+'!')
+console.log('Thanks '+name+'! Let\'s take a look at what we\'ve come up with. Filling up at the first station will cost you $'+costOne+'. If you drive to the second gas station, you will burn an additional '+gasDiff+' gallon(s) of fuel. The cost to fill up at the second station will be $'+costTwo+'. That\'s a difference of $'+difference+'!')
+
+/*
+I used a gas tank size of 10 gal with 0.5 of the tank remaining and a first station price of $2.50.
+This gave me the price for the first station as $12.50.
+The distance I used was 5 miles and mileage of 20 mpg for an additional 0.25 gallons used and a new amount of gas needed of 5.25 gal.
+I used $3.00 as the price at the second station for a total of $15.75 at the second station. The difference is of course $3.75.
+
+For the second set I used a tank size of 13 gal, 0.33 of the tank remaining and a first price of $2.67.
+This gave me a first station price of $23.26 after rounding.
+I used a distance of 3 miles and mileage of 18 mpg for an additional 0.17 gallons (after rounding) used and a total amount needed of roughly 8.88 gal.
+I used $2.58 for the second station price for a rounded second total of $22.90 and a difference of $0.36.
+ */
