4227958967f33fd101874b37faf7e68d3cce258a
Craig Basol <cbasol@fullsail.edu>
2015-05-21 21:32:18 -0400

    Added first variable prompts
    Created info for first golfer

--- Basol_Craig_Conditionals_Assignment/js/script.js
+++ Basol_Craig_Conditionals_Assignment/js/script.js
@@ -6,12 +6,29 @@ Section 01
 Conditionals Assignment
 */
 
+// Calculate greens fees for a golf party
+
  // Ask user's name before anything
 var user = prompt('What is your name?');
 // Print to console
 console.log(user);
 
-// Validate name
-(user === '') ? user=prompt('Can you at least eneter a fake name?') : ;
+// Validate user name
+if (user === ''){
+ user=prompt('Can you at least enter a fake name?');
+}
 // Print to console
 console.log(user);
+
+
+// Ask for first golfer's info
+var golferOne = [];
+var golferOneName = prompt('What is the first golfer\'s name?');
+if (golferOneName === '' || !(isNaN(golferOneName))){
+golferOneName=prompt('Can you at least enter a fake name?');
+}
+var golferOneMember = prompt('Is this person a member at the course?');
+golferOne.push(golferOneName);
+golferOne.push(golferOneMember);
+// Print to console
+console.log(golferOne);
