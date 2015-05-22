745ba858cc654eed42f2b9a1269ddf309f6342bf
Craig Basol <cbasol@fullsail.edu>
2015-05-21 22:14:14 -0400

    Finished golfer variables
    Completed prompts for rest of golf party

--- Basol_Craig_Conditionals_Assignment/js/script.js
+++ Basol_Craig_Conditionals_Assignment/js/script.js
@@ -40,3 +40,57 @@ golferOne.push(golferOneMember);
 console.log(golferOne);
 console.log(members);
 console.log(guests);
+
+// Ask for second golfer's info
+var golferTwo = [];
+var golferTwoName = prompt('What is the second golfer\'s name?');
+if (golferTwoName === '' || !(isNaN(golferTwoName))){
+ golferTwoName = prompt('Can you at least enter a fake name?');
+}
+var twoMember = prompt('Is this person a member at the course?');
+var golferTwoMember = twoMember.toLocaleLowerCase();
+// Print to console
+console.log(golferTwoMember);
+(golferTwoMember === 'yes') ? members++ : guests++ ;
+golferTwo.push(golferTwoName);
+golferTwo.push(golferTwoMember);
+// Print to console
+console.log(golferTwo);
+console.log(members);
+console.log(guests);
+
+// Ask for first golfer's info
+var golferThree = [];
+var golferThreeName = prompt('What is the third golfer\'s name?');
+if (golferThreeName === '' || !(isNaN(golferThreeName))){
+ golferThreeName = prompt('Can you at least enter a fake name?');
+}
+var threeMember = prompt('Is this person a member at the course?');
+var golferThreeMember = threeMember.toLocaleLowerCase();
+// Print to console
+console.log(golferThreeMember);
+(golferThreeMember === 'yes') ? members++ : guests++ ;
+golferThree.push(golferThreeName);
+golferThree.push(golferThreeMember);
+// Print to console
+console.log(golferThree);
+console.log(members);
+console.log(guests);
+
+// Ask for fourth golfer's info
+var golferFour = [];
+var golferFourName = prompt('What is the fourth golfer\'s name?');
+if (golferFourName === '' || !(isNaN(golferFourName))){
+ golferFourName = prompt('Can you at least enter a fake name?');
+}
+var fourMember = prompt('Is this person a member at the course?');
+var golferFourMember = fourMember.toLocaleLowerCase();
+// Print to console
+console.log(golferFourMember);
+(golferFourMember === 'yes') ? members++ : guests++ ;
+golferFour.push(golferFourName);
+golferFour.push(golferFourMember);
+// Print to console
+console.log(golferFour);
+console.log(members);
+console.log(guests);
