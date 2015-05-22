ad7b728745989033e116e0b9108d00120b179b80
Craig Basol <cbasol@fullsail.edu>
2015-05-21 20:44:09 -0400

    Created Files
    File creation and user name variable/validation

---
+++ Basol_Craig_Conditionals_Assignment/js/script.js
@@ -0,0 +1,17 @@
+
+/*
+Craig Basol
+5/21/15
+Section 01
+Conditionals Assignment
+*/
+
+ // Ask user's name before anything
+var user = prompt('What is your name?');
+// Print to console
+console.log(user);
+
+// Validate name
+(user === '') ? user=prompt('Can you at least eneter a fake name?') : ;
+// Print to console
+console.log(user);
