d53b67a23f69f2fec9a9b476ec18ae0375ac555b
Craig Basol <cbasol@fullsail.edu>
2015-05-28 18:40:43 -0400

    Created Files
    Created and linked files

---
+++ Basol_Craig_Functions_Assignment/js/script.js
@@ -0,0 +1,11 @@
+/*
+ Craig Basol
+ 5/28/15
+ Section 01
+ Functions Assignment
+ */
+
+// Simple cafe register-style calculation... something
+
+alert('\'Ello there, name\'s Pip! Welcome to the cafe!');
+
