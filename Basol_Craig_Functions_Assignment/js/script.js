158a442f681b7658eba303750298d913615d19a7
Craig Basol <cbasol@fullsail.edu>
2015-05-28 19:05:49 -0400

    First Variables
    Created basic outline for drink orders

--- Basol_Craig_Functions_Assignment/js/script.js
+++ Basol_Craig_Functions_Assignment/js/script.js
@@ -17,3 +17,24 @@ while (foodOrDrinks != 'food' && foodOrDrinks != 'drinks'){
     foodOrDrinks.toLowerCase();
 }
 
+var subtotal = 0;
+
+if (foodOrDrinks === 'drinks'){
+    drinks(subtotal);
+};
+
+function drinks(total){
+    alert('Great! We\'ve got coffee and tea for $3, soda and juice for $2.50, and wine and beer for $6.');
+    var order = prompt('What can I do ya for?');
+    order.toLowerCase();
+    if (order === 'coffee' || order === 'tea'){
+        total += 3;
+    } else if (order === 'soda' || order === 'juice'){
+        total += 2.5;
+    } else if (order === 'wine' || order === 'beer'){
+        total += 6;
+    } else {
+        order = prompt('Take yer time. As a reminder:\nWe\'ve got coffee and tea for $3, soda and juice for $2.50, and wine and beer for $6.');
+        order.toLowerCase();
+    }
+};
