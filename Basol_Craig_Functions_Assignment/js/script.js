e1687877ee3b4d66686b6e8f005c2d27458c5d2a
Craig Basol <cbasol@fullsail.edu>
2015-05-28 19:33:13 -0400

    Fixes and loops
    Fixes to OOO, slight cleanup, started order loop

--- Basol_Craig_Functions_Assignment/js/script.js
+++ Basol_Craig_Functions_Assignment/js/script.js
@@ -21,20 +21,29 @@ var subtotal = 0;
 
 if (foodOrDrinks === 'drinks'){
     drinks(subtotal);
-};
+}
 
 function drinks(total){
     alert('Great! We\'ve got coffee and tea for $3, soda and juice for $2.50, and wine and beer for $6.');
     var order = prompt('What can I do ya for?');
     order.toLowerCase();
-    if (order === 'coffee' || order === 'tea'){
-        total += 3;
-    } else if (order === 'soda' || order === 'juice'){
-        total += 2.5;
-    } else if (order === 'wine' || order === 'beer'){
-        total += 6;
-    } else {
+    var drinkTotal = 0;
+    var drinkTotal = drinkOrder(order, total);
+    var more = prompt('Can I get ya anythin\' else?\n(Please answer yes or no)');
+    more.toLowerCase();
+    
+}
+
+function drinkOrder(order, orderTotal){
+    while (order != 'coffee' && order != 'tea' && order != 'soda' && order != 'juice' && order != 'wine' && order != 'beer'){
         order = prompt('Take yer time. As a reminder:\nWe\'ve got coffee and tea for $3, soda and juice for $2.50, and wine and beer for $6.');
         order.toLowerCase();
+    } if (order === 'coffee' || order === 'tea'){
+        orderTotal += 3;
+    } else if (order === 'soda' || order === 'juice'){
+        orderTotal += 2.5;
+    } else if (order === 'wine' || order === 'beer'){
+        orderTotal += 6;
     }
-};
+    return orderTotal
+}
